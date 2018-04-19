55a3a2a245c607e7e86e8395520d5e946427f5f5
saboudreault <saboudreault@gmail.com>
2018-04-19 14:12:33 -0400

    + ludiq + protools + first person drifter + some standard assets + menu scene & gym scene + menu navigation script

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	private bool isMenu;
+
+	void Start(){
+		if (Application.loadedLevel == 0) {
+			Cursor.visible = true;
+			isMenu = true;
+			print ("yas");
+		}
+	}
+
+	void Update(){
+		if (Input.GetKey ("escape")) {
+			if (isMenu) {
+				Application.Quit ();
+			} else {
+				SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+			}
+		}
+	}
+
+	public void LoadSceneGym(){
+		SceneManager.LoadScene("Gym", LoadSceneMode.Single);
+	}
+}
