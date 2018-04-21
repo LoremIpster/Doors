28cbd0fae5845b2596c64d8375216ceda4fc4fa4
Unknown <collectivisation@gmail.com>
2018-04-21 12:07:05 -0500

    Edge detection working

--- Assets/Scripts/FX/ColorLerpInTriggerVolume.cs
+++ Assets/Scripts/FX/ColorLerpInTriggerVolume.cs
@@ -1,16 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityStandardAssets.ImageEffects;
+[ExecuteInEditMode, RequireComponent(typeof(BoxCollider))]
 public class ColorLerpInTriggerVolume : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public float m_size;
+    public Color m_newColor = Color.red;
+    BoxCollider m_box;
+    EdgeDetectionColor edgeDetection;
+
+    [Range(.1f, 5f), SerializeField]
+    float m_lerpDuration;
+
+    private void Awake()
+    {
+        m_box = GetComponent<BoxCollider>();
+        edgeDetection = Camera.main.GetComponent<EdgeDetectionColor>();
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(m_newColor.r, m_newColor.g, m_newColor.b, .2f);
+        Gizmos.DrawCube(transform.position, Vector3.one * m_size);
+        m_box.size = new Vector3(m_size, m_size, m_size);
+    }
+    
+
+    // Update is called once per frame
+    void OnTriggerEnter (Collider coll) {
+        if (coll.tag == "Player")
+        {
+            StartCoroutine(Lerp());
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    IEnumerator Lerp()
+    {
+        float elapsed = 0;
+        while (elapsed <= m_lerpDuration)
+        {
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+    }
 }
