ea6aca824d689fc38560951e761c00c5606e059d
Unknown <collectivisation@gmail.com>
2018-05-06 15:00:34 -0500

    First scene WIP + Cleanup

---
+++ Assets/Scripts/TriggerObjectActivation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class TriggerObjectActivation : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject m_objToDestroy;
+
+    // Use this for initialization
+    void OnTriggerEnter()
+    {
+        Destroy(m_objToDestroy);
+    }
+}
---
+++ Assets/Scripts/VisibilityTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisibilityTrigger : MonoBehaviour
+{
+
+    [SerializeField]
+    float m_triggerTime;
+    Coroutine countdown;
+    // Update is called once per frame
+    void OnBecameVisible()
+    {
+        countdown = StartCoroutine(StartCountdown());
+    }
+
+    IEnumerator StartCountdown()
+    {
+        float elapsed = 0;
+        while (elapsed < m_triggerTime)
+        {
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        Trigger();
+    }
+
+    protected virtual void Trigger()
+    {
+        StopCoroutine(countdown);
+        countdown = null;
+    }
+
+    private void OnBecameInvisible()
+    {
+        m_triggerTime = 0;
+    }
+}
