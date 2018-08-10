cb16817595a3b5ed422b2c927f19caba633fb76c
Simon-Albert Boudreault <saboudreault@gmail.com>
2018-08-10 23:53:49 +1000

    ENDING FIX
    rerajouté le script nommé "Ending" à la toute fin. changé le AA. modifié les color lerps à la fin.

---
+++ Assets/Scripts/Ending.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ending : MonoBehaviour
+{
+
+    private GazeCheck gazeScript;
+    private PositionCheck positionScript;
+    private PositionCheck restartScript;
+    private bool textDisplayed;
+    private bool staircaseReady;
+
+    public GameObject triggerEnding;
+    public GameObject triggerRestart;
+    public GameObject gazeObject;
+    public GameObject textEnding;
+    public GameObject collisionFront;
+    public GameObject collisionBack;
+    public GameObject finalStaircaseFirstHalf;
+    public GameObject finalStaircaseSecondHalf;
+
+    void Start()
+    {
+        gazeScript = gazeObject.GetComponent<GazeCheck>();
+        positionScript = triggerEnding.GetComponent<PositionCheck>();
+        restartScript = triggerRestart.GetComponent<PositionCheck>();
+
+        textDisplayed = false;
+        staircaseReady = false;
+    }
+
+    void Update()
+    {
+        StaircaseSwitch();
+        Reset();
+    }
+
+    private void StaircaseSwitch()
+    {
+        if (!gazeScript.isVisible && positionScript.isInside && !textDisplayed)
+        {
+            textEnding.SetActive(true);
+            finalStaircaseFirstHalf.SetActive(false);
+            staircaseReady = true;
+            textDisplayed = true;
+        }
+
+        if (gazeScript.isVisible && positionScript.isInside && staircaseReady)
+        {
+            finalStaircaseSecondHalf.SetActive(true);
+            collisionFront.SetActive(false);
+        }
+
+        if (!gazeScript.isVisible && positionScript.isInside && textDisplayed)
+        {
+            textEnding.SetActive(true);
+            collisionBack.SetActive(true);
+        }
+    }
+
+    private void Reset()
+    {
+        if (restartScript.isInside)
+        {
+            textDisplayed = false;
+            staircaseReady = false;
+            finalStaircaseFirstHalf.SetActive(true);
+            finalStaircaseSecondHalf.SetActive(false);
+            collisionBack.SetActive(false);
+            collisionFront.SetActive(true);
+        }
+    }
+
+}
